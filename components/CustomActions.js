da41bacf768b3ab78893ec222124939d2f0f6ae9
Nico Vece <me@nicovece.com>
2025-05-25 10:54:19 +0200

    Enables camera functionality
    Implements the camera feature, allowing users to take and upload photos.
    
    This change addresses the previous commented-out code, enabling camera access and image uploading functionality. An alert informs the user if permissions are not granted.

--- components/CustomActions.js
+++ components/CustomActions.js
@@ -56,15 +56,12 @@ const CustomActions = ({
 
   // Take photo
   const takePhoto = async () => {
-    // Example usage:
-    // let permissions = await ImagePicker.requestCameraPermissionsAsync();
-    // if (permissions?.granted) {
-    //   let result = await ImagePicker.launchCameraAsync();
-    //   if (!result.canceled) {
-    //     const imageURI = result.assets[0].uri;
-    //     await uploadAndSendImage(imageURI);
-    //   }
-    // }
+    let permissions = await ImagePicker.requestCameraPermissionsAsync();
+    if (permissions?.granted) {
+      let result = await ImagePicker.launchCameraAsync();
+      if (!result.canceled) await uploadAndSendImage(result.assets[0].uri);
+      else Alert.alert("Permissions haven't been granted");
+    }
   };
 
   // Get location
